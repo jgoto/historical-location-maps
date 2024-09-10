98a21f9eaf922593a23bde3d11f370dc9be1dc17
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-09-09 20:32:07 -0400

    Finished adding the map and test markers

--- src/script.js
+++ src/script.js
@@ -1,5 +1,14 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([38.7293, -77.1074], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+const marker = L.marker([38.7293, -77.1074]).addTo(map);
+const circle = L.circle([38.7277, -77.1062],{
+    color: "red",
+    fillCOlor: "#f83",
+    fillOpacity: 0.5,
+    radius: 300
+}).addTo(map);
+circle.bindPopup("Mt Vernon VA");
